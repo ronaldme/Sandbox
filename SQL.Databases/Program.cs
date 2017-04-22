81721c611005392d744c404b4f004cd06331e00d
Ronald Meijboom <ronaldme@gmail.com>
2017-04-22 09:45:21 +0200

    Add SQL.Databases project
    Retrieve SQL database names

---
+++ SQL.Databases/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQL.Databases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbNames = GetDatabaseNames();
+            dbNames.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+        }
+
+        private static List<string> GetDatabaseNames()
+        {
+            var dbNames = new List<string>();
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["sql-server-db"].ConnectionString))
+            {
+                con.Open();
+                var databases = con.GetSchema("Databases");
+
+                foreach (DataRow database in databases.Rows)
+                {
+                    string databaseName = database.Field<string>("database_name");
+                    dbNames.Add(databaseName);
+                }
+            }
+
+            return dbNames;
+        }
+    }
+}
