58b50358a025b7cb774280e4f4e2c43943bbae7c
ronaldme <ronaldme@gmail.com>
2017-04-24 10:08:55 +0200

    Create backup of databases

--- SQL.Databases/Program.cs
+++ SQL.Databases/Program.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace SQL.Databases
 {
     class Program
     {
+        private static readonly string dbPath = @"D:\databases";
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["sql-server-db"].ConnectionString;
+
         static void Main(string[] args)
         {
             var dbNames = GetDatabaseNames();
@@ -18,14 +23,43 @@ namespace SQL.Databases
 
             dbNames.ForEach(Console.WriteLine);
 
+            Console.WriteLine("Starting to backup databases...");
+            BackupDatabases(dbNames);
+
+            Console.WriteLine("Done");
             Console.ReadLine();
+        }
+
+        private static void BackupDatabases(List<string> databaseNames)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            foreach (var dbName in databaseNames)
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    Directory.CreateDirectory($@"{dbPath}\{dbName}");
+
+                    var date = DateTime.Now.ToString("yyyy-MM-dd-HH-mm");
+                    var cmd = new SqlCommand
+                    {
+                        CommandText = $@"BACKUP DATABASE ""{dbName}"" TO DISK = '{dbPath}\{dbName}\{date}-{dbName}.bak'",
+                        CommandType = CommandType.Text,
+                        Connection = connection
+                    };
+
+                    connection.Open();
+                    cmd.ExecuteReader();
+                    connection.Close();
+                }
+            }
+            Console.WriteLine($"Created database backups in: {stopWatch.ElapsedMilliseconds}ms.");
         }
 
         private static List<string> GetDatabaseNames()
         {
             var dbNames = new List<string>();
 
-            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["sql-server-db"].ConnectionString))
+            using (var con = new SqlConnection(connectionString))
             {
                 con.Open();
                 var databases = con.GetSchema("Databases");
